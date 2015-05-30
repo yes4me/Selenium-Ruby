935fef7911ab010f4f77b9c860ff461ce8499d6b
yes4me <yes4me@hotmail.com>
2015-05-30 09:24:29 -0700

    no message

--- helper/signuptrial_helper.rb
+++ helper/signuptrial_helper.rb
@@ -3,6 +3,7 @@
 # Purpose: remove redundant codes
 # ================================================================
 
+require_relative FileNames::LIB_MY_EMAIL
 require_relative FileNames::PAGES_SIGNUP_TRIAL
 
 
@@ -10,6 +11,10 @@ class SignupTrialHelper < SignupTrial
 	#Optional hash parameter so that you can override one or more defaults if needed
 	def test(pass, parameters = {})
 		visit
+
+		if !(defined?(parameters[:email])).nil?
+			parameters[:email] = MyEmail.new("selenium.automation@saucelabs.com").gen_unique_email_address
+		end
 
 		type_user_info(parameters)
 		type_company_info(parameters)
